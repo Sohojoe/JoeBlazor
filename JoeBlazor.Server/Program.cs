dd738226996f4f3daf50d849361fcd220885e518
Joe Booth <joe@joebooth.com>
2022-07-05 21:23:03 -0700

    Wasm uses shared

--- JoeBlazor.Server/Program.cs
+++ JoeBlazor.Server/Program.cs
@@ -11,6 +11,24 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddMudServices();
 
+// Server Side Blazor doesn't register HttpClient by default
+// Thanks to Robin Sue - Suchiman https://github.com/Suchiman/BlazorDualMode
+//   and to https://www.codeproject.com/Articles/5299017/Building-a-Blazor-WASM-and-Server-All-In-One-Solut
+if (!builder.Services.Any(x => x.ServiceType == typeof(HttpClient)))
+{
+    // Setup HttpClient for server side in a client side compatible fashion
+    builder.Services.AddScoped<HttpClient>(s =>
+    {
+        // Creating the URI helper needs to wait until the 
+        // JS Runtime is initialized, so defer it.
+        var uriHelper = s.GetRequiredService<NavigationManager>();
+        return new HttpClient
+        {
+            BaseAddress = new Uri(uriHelper.BaseUri)
+        };
+    });
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
